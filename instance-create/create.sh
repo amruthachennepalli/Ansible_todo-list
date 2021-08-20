6cd9dd8f7289fcd872611154ecf3d571e8f28477
Amrutharaj <amrutha@zelarsoft.com>
2021-08-20 19:42:22 +0530

    ss

--- instance-create/create.sh
+++ instance-create/create.sh
@@ -9,7 +9,6 @@ if [ -z "$1" ]; then
   echo "Component Name INput is needed"
   exit 1
 fi
-
 Instance_Create() {
   COMPONENT=$1
   INSTANCE_EXISTS=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${COMPONENT}  | jq .Reservations[])
@@ -19,16 +18,12 @@ Instance_Create() {
   else
     echo "Instance ${COMPONENT} already exists"
   fi
-
-
   IPADDRESS=$(aws ec2 describe-instances     --filters Name=tag:Name,Values=${COMPONENT}   | jq .Reservations[].Instances[].PrivateIpAddress | grep -v null |xargs)
-
   sed -e "s/COMPONENT/${COMPONENT}/" -e "s/IPADDRESS/${IPADDRESS}/" record.json >/tmp/record.json
   aws route53 change-resource-record-sets --hosted-zone-id Z02614382HY5QU7JW60C9 --change-batch file:///tmp/record.json
   sed -i -e "/${COMPONENT}/ d" ../inv
   echo "${IPADDRESS} COMPONENT=$(echo ${COMPONENT} | awk -F - '{print $1}')" >>../inv
 }
-
 if [ "$1" == "all" ]; then
   for instance in frontend todo login users redis ; do
     Instance_Create $instance-dev
@@ -36,6 +31,5 @@ if [ "$1" == "all" ]; then
 else
   Instance_Create $1-dev
 fi
-
 # AKIA4UOVAQ7BMC3R47P6
 # 5Wcq2pUu97njYWJYfrFnWabafnU9z4b9PmqufA11 
